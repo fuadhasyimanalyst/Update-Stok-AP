3b2d8c69daf99e322df2bd5f4d98a5a3053c7e07
fuadhasyimanalyst <fuadhasyimanalyst@gmail.com>
2026-08-07 08:55:11 +0700

    perubahan besar

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
+    <html lang="id" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
